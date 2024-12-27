2dd86f60f4b0f59f9f41638babfb8f92a1551b53
Chi Xu <xuchi007@hotmail.com>
2024-12-27 17:39:01 +1300

    update banner

--- components/BlackFridayBanner.tsx
+++ components/BlackFridayBanner.tsx
@@ -11,7 +11,27 @@ async function BlackFridayBanner() {
 
   return (
     <div className='bg-gradient-to-r from-red-600 to-black text-white px-6 py-10 mx-4 mt-2 rounded-lg shadow-lg'>
-      Black Friday Banner
+      <div className='container mx-auto flex items-center justify-between'>
+        <div className='flex-1'>
+          <h2 className='text-3xl sm:text-5xl font-extrabold text-left mb-6'>
+            {sale.title}
+          </h2>
+          <p className='text-left text-xl sm:text-3xl font-semibold mb-6'>
+            {sale.description}
+          </p>
+
+          <div className='flex'>
+            <div className='bg-white text-black px-4 py-2 rounded-full
+              shadow-md transform hover:scale-105 transition duration-300'>
+              <span className='font-bold text-base sm:text-xl'>
+                Use Code:{" "}
+                <span className='text-red-600'>{sale.couponCode}</span>
+                <span className='ml-2 font-bold text-base sm:text-xl'>for {sale.discountAmount}% OFF</span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
