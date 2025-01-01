168a140cd96644d4d8f3f8417e571d60fd65545f
Chi Xu <xuchi007@hotmail.com>
2025-01-01 17:48:36 +1300

    bug fix for compile

--- app/(store)/search/page.tsx
+++ app/(store)/search/page.tsx
@@ -5,9 +5,7 @@ import React from 'react'
 async function SearchPage({
   searchParams,
 }: {
-  searchParams: {
-    query: string;
-  };
+  searchParams: Promise<{ query: string }>;
 }) {
   const { query } = await searchParams;
   const products = await searchProductsByName(query);
